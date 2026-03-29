fcc6f58e3c538aff5eadf74f8ce914fcc9b39352
addy <addyjaime@gmail.com>
2026-03-29 11:26:04 -0400

    added all stadium to the db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg"
 
-const prisma = new PrismaClient();
+
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
+})
+
+export const prisma = new PrismaClient({
+  adapter
+})
 
 async function main() {
 
